0369a2e0926af399f6b10ad5462e4012d39ad022
Saravanan Vijayakumar <brsaran@gmail.com>
2016-03-04 09:54:16 +0530

    Delete clear_fuzz_jobs.pl

--- clear_fuzz_jobs.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl -w
-######## Sub Chloroplast Loclaization using ADABOOST predictor (S-CLAP)#####################
-#        Developed by SARAVANAN VIJAYAKUMAR
-#         Centre for Bioinformatics, Pondicherry University
-#         brsaran@gmail.com
-#         Date: 15-03-2012
-#
-
-`rm -f /var/www/html/fuzzyapp/temp/*.fuzz`;
